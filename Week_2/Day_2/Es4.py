cddc10ec07ccd42b063b5586b378892694b20e1e
RofferTorres <114493642+RofferTorres@users.noreply.github.com>
2022-10-04 16:54:02 +0200

    Update Exercises Day 2

---
+++ Week_2/Day_2/Es4.py
@@ -0,0 +1,12 @@
+#Esercizio 4
+Metri = float(input('Scrivi la misura da convertire: '))
+#1 miglio = 1609.344 metri
+Miglia = Metri /1609.344
+#1 piede = 0.3048 metri
+Piede = Metri /0.3048
+#1 pollice = 0.0254
+Pollice = Metri/ 0.0254
+
+print('Valore in miglia è: ',Miglia,'mi')
+print('Valore in piedi è: ',Piede,'ft')
+print('Valore in pollice è: ',Pollice,'in')
---
+++ Week_2/Day_2/Es5.py
@@ -0,0 +1,4 @@
+#Esercizio 5
+var = input('Scrivi qualcosa: ')
+
+print(var[0:3]+'...'+var[-3:])  #stampa primi 3 caratteri + '...'+ultimi 3 caratteri
