640cf648ddc09baed4be2aa35b08577cd1080358
RofferTorres <114493642+RofferTorres@users.noreply.github.com>
2022-10-06 01:04:17 +0200

    Update Es_2.py

--- Week_2/Day_3/Es_2.py
+++ Week_2/Day_3/Es_2.py
@@ -20,10 +20,11 @@ print('"Worst buyer" ==> {}: {}€ \n'.format(matrix[0][i_min].capitalize(), mat
 # media
 avarage = sum(matrix[1])/len(matrix[1])
 print('Customers spent on average: € {}\n'.format(avarage))
-# mediana""""
-len= int(len(matrix[1]))
-if len %2 == 0 :
-    median = (matrix[1][int(len/2)] + matrix[1][int(len/2 - 1)])/2
+# mediana
+len = int(len(matrix[1]))
+sort_price = matrix[1][0:len]           # lista ordinata dei prezzi
+if len %2 == 0 :                        #se dati pari faccio media tra valori di indici len/2 e len/2 - 1
+    median = (sort_price[int(len/2)] + sort_price[int(len/2 - 1)])/2
 else:
-    median = matrix[1][int(((len - 1)/2))]
+    median = sort_price[int(((len - 1)/2))]     #se dati dispari prendo la metà del valore centrale nella lista ordinata
 print('Median is: € {}\n'.format(median))
